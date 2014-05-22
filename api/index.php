c27f3a2359e2a3946b4e6745ea25827fef63e828
Pala <paladinop@gmail.com>
2014-05-21 22:26:04 -0300

    frontend board binding + random backend picker

--- api/index.php
+++ api/index.php
@@ -1,2 +1,64 @@
 <?php
+session_start();
+error_reporting(2);
+
 /*API*/
+class GameRequest
+{
+    function __construct($params) {
+    	$this->default_moves = array(
+    		 '00' => array(0,0)
+    		,'01' => array(0,1)
+    		,'02' => array(0,2)
+    		,'10' => array(1,0)
+    		,'11' => array(1,1)
+    		,'12' => array(1,2)
+    		,'20' => array(2,0)
+    		,'21' => array(2,1)
+    		,'22' => array(2,2)
+    		);
+    	$this->action = $params['action'];
+	   	$this->position = $params['position'];
+	   	$this->success = false;
+   	}
+
+   	function run(){
+   		if($this->action == 'clear'){
+   			$this->_clear();
+   		} else if($this->action == 'move') {
+   			$this->_saveMove($this->position);
+   			$this->_pickRandomMove();
+   		}
+   	}	
+
+   	function _saveMove($position){
+   		unset($_SESSION['moves'][$position[0].$position[1]]);
+   	}	
+
+   	function _clear(){
+   		$_SESSION['moves'] = $this->default_moves;
+   		$this->success = true;
+   	}	
+
+   	function _pickRandomMove(){
+   		$randomMove = $_SESSION['moves'][array_rand($_SESSION['moves'],1)];
+   		$this->cpuMove = $randomMove;
+   		$this->_saveMove($randomMove);
+   		$this->success = true;
+   	}	
+
+   	function render(){
+   		$resp = array(
+   			'position' => $this->cpuMove,
+   			'success' => $this->success,
+   			'moves' => $_SESSION['moves']
+   			);
+   		return $resp;
+   	}
+}
+
+$gameRequest = new GameRequest($_POST);
+
+$gameRequest->run();
+
+echo json_encode($gameRequest->render());
--- js/main.js
+++ js/main.js
@@ -9,14 +9,65 @@ var TicTacToe;
 
     TicTacToe = global.TicTacToe = global.TicTacToe || {};
 
-    //Var definitions
-    this.bindings = {
+    TicTacToe.initialBoard = [
+            ko.observableArray([ko.observable(undefined),ko.observable(undefined),ko.observable(undefined)]),
+            ko.observableArray([ko.observable(undefined),ko.observable(undefined),ko.observable(undefined)]),
+            ko.observableArray([ko.observable(undefined),ko.observable(undefined),ko.observable(undefined)])
+            ];
 
+    //Var definitions
+    TicTacToe.bindings = {
+        board : ko.observableArray(TicTacToe.initialBoard),
+        loading: ko.observable(false)
     };
 
     //methods
-    this.init = function () {
-        ko.applyBindings(TicTacToe.bindings);
+    TicTacToe.init = function () {
+        //Initialize board tiles click
+        $("#main-content").on("click", ".board-tile", function() {
+            var $obj = $(this);
+            if(TicTacToe.bindings.loading() === false && $obj.hasClass('clickeable')){
+                TicTacToe.userMove($(this).attr('rel').split('|'));
+            }
+        });
+
+        //Clear session
+        TicTacToe.apiCall({ action:'clear' },
+            function(data) {
+                ko.applyBindings(TicTacToe.bindings);
+                TicTacToe.bindings.loading(false);
+            });
+    };
+
+    TicTacToe.userMove = function(position){
+        this.bindings.loading(true);
+        this.setTileValue(position,'icon-radio-unchecked');
+        this.cpuMove(position);
+    };
+
+    TicTacToe.cpuMove = function(position){
+        var that = this;
+
+        TicTacToe.apiCall({ action:'move', position: position },
+            function(data) {
+                that.bindings.loading(false);
+                that.setTileValue(data.position,'icon-cancel-2');
+                console.log(data);
+            });
+    };
+    
+    TicTacToe.apiCall = function(params,callback){
+        $.post("api/index.php", params, 
+            function(data, textStatus, jqXHR) {
+                callback(data);
+            },"json")
+            .fail(function(jqXHR, textStatus, errorThrown) {
+                console.error(textStatus);
+            });
+    };
+
+    TicTacToe.setTileValue = function (position,value) {
+        TicTacToe.bindings.board()[position[0]]()[position[1]](value);
     };
 
 })(window, document,jQuery,ko);
